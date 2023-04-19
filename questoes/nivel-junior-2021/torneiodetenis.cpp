f0a137bbed0f87271711f61ebf1ad3d3b365ad7a
davisantx <davisant8881@gmail.com>
2023-04-19 18:11:41 -0300

    add "add torneiodetenis.cpp (new)"

--- questoes/nivel-junior-2021/torneiodetenis.cpp
+++ questoes/nivel-junior-2021/torneiodetenis.cpp
@@ -5,32 +5,25 @@
 
 using std::cout, std::cin;
 
-int retornarGrupo(std::vector<char> situacao) {
+int retornarGrupo(std::vector<char> situacao, std::vector<int> grupo) {
     unsigned quantidadeDeVitorias = 0;
     for(int i = 0; i < situacao.size(); ++i) {
       if(situacao[i] == 'V' || situacao[i] == 'v') ++quantidadeDeVitorias;
     }
-    if(quantidadeDeVitorias == 0) {
-        return -1;
-    }else if(quantidadeDeVitorias <= 2) {
-        return 3;
-    }else if(quantidadeDeVitorias <= 4) {
-        return 2;
-    }else if(quantidadeDeVitorias <= 6) {
-        return 1;
-    }
+    return grupo[quantidadeDeVitorias];
 }
 
 int main() {
 
    std::vector<char> situacao (6, ' ');
+   std::vector<int> grupo = {-1, 3, 3, 2, 2, 1, 1};
 
    for(int i = 0; i < situacao.size(); ++i) {
     cout << "Resultado do jogo " << i + 1 << ": ";
     cin >> situacao[i];
    }
 
-   cout << "Grupo pertencente: " << retornarGrupo(situacao);
+   cout << "Grupo pertencente: " << retornarGrupo(situacao, grupo);
 
-    return 0;
+   return 0;
 }
